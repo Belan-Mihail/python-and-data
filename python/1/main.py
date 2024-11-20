4555416ee9dac67955d65cb9783af2701832bc75
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-11-20 21:05:59 +0100

    create acronyms func

--- python/1/main.py
+++ python/1/main.py
@@ -1,1 +1,20 @@
-print("hello world!")
+# projects from https://thecleverprogrammer.com/2021/01/13/acronyms-using-python/
+
+
+
+
+
+
+
+#  1 Create Acronyms using Python
+
+user_input = str(input("Enter you Phrase: "))
+
+text = user_input.split()
+
+a = ""
+
+for i in text:
+    a = a + str(i[0]).upper()
+
+print(a)

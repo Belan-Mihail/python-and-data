552cbb56e98a29af366a0cfeb4f231d3d87af2dd
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-11-20 21:22:46 +0100

    create alarm clock

--- python/1/main.py
+++ python/1/main.py
@@ -1,6 +1,34 @@
 # projects from https://thecleverprogrammer.com/2021/01/13/acronyms-using-python/
 
 
+# 2 Alarm Clock
+
+from datetime import datetime
+
+alarm_time = input("Enter the time of alarm to be set:HH:MM:SS\n")
+
+alarm_hour = alarm_time[0:2]
+
+alarm_minute=alarm_time[3:5]
+
+alarm_second=alarm_time[6:8]
+
+
+while True:
+
+    now = datetime.now()
+    
+    current_hour = now.strftime('%I')
+    
+    current_minute = now.strftime('%M')
+    
+    current_second = now.strftime('%S')
+    
+    if(alarm_hour == current_hour):
+        if(alarm_minute == current_minute):
+            if (alarm_second == current_second):
+                print('Wake up')
+                break
 
 
 
@@ -8,13 +36,13 @@
 
 #  1 Create Acronyms using Python
 
-user_input = str(input("Enter you Phrase: "))
+# user_input = str(input("Enter you Phrase: "))
 
-text = user_input.split()
+# text = user_input.split()
 
-a = ""
+# a = ""
 
-for i in text:
-    a = a + str(i[0]).upper()
+# for i in text:
+#     a = a + str(i[0]).upper()
 
-print(a)
+# print(a)

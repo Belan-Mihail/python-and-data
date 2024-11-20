b625fbb698f9e4f1e954939034731b51c699f526
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-11-20 21:39:00 +0100

    add separeter between function

--- python/1/main.py
+++ python/1/main.py
@@ -1,39 +1,43 @@
 # projects from https://thecleverprogrammer.com/2021/01/13/acronyms-using-python/
 
 
+
+# ##################################################################################
+# 3 Email Slicer
+
+
+# ##################################################################################
 # 2 Alarm Clock
 
-from datetime import datetime
+# from datetime import datetime
 
-alarm_time = input("Enter the time of alarm to be set:HH:MM:SS\n")
+# alarm_time = input("Enter the time of alarm to be set:HH:MM:SS\n")
 
-alarm_hour = alarm_time[0:2]
+# alarm_hour = alarm_time[0:2]
 
-alarm_minute=alarm_time[3:5]
+# alarm_minute=alarm_time[3:5]
 
-alarm_second=alarm_time[6:8]
+# alarm_second=alarm_time[6:8]
 
 
-while True:
+# while True:
 
-    now = datetime.now()
+#     now = datetime.now()
     
-    current_hour = now.strftime('%I')
+#     current_hour = now.strftime('%I')
     
-    current_minute = now.strftime('%M')
+#     current_minute = now.strftime('%M')
     
-    current_second = now.strftime('%S')
+#     current_second = now.strftime('%S')
     
-    if(alarm_hour == current_hour):
-        if(alarm_minute == current_minute):
-            if (alarm_second == current_second):
-                print('Wake up')
-                break
+#     if(alarm_hour == current_hour):
+#         if(alarm_minute == current_minute):
+#             if (alarm_second == current_second):
+#                 print('Wake up')
+#                 break
 
 
-
-
-
+# ##################################################################################
 #  1 Create Acronyms using Python
 
 # user_input = str(input("Enter you Phrase: "))

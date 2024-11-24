3bdac21f60650687e692cec720025861a454f797
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-11-24 21:07:19 +0100

    create sales_by_date_df_five and plot

--- pandas_and_numpy/sales_data_analyse/main.py
+++ pandas_and_numpy/sales_data_analyse/main.py
@@ -176,12 +176,20 @@ df_five = df[df['Company'].isin(five_companies)]
 #     print("-" * 20)
 
 #  27 sales_by_region_company df_five
-sales_by_region_company = df_five.groupby(['Company', 'Region'])['Sales'].sum().unstack()
+# sales_by_region_company = df_five.groupby(['Company', 'Region'])['Sales'].sum().unstack()
 # print(sales_by_region_company)
 # print("-" * 20)
-sales_by_region_company = sales_by_region_company.fillna(0)
-print(sales_by_region_company)
+# sales_by_region_company = sales_by_region_company.fillna(0)
+# print(sales_by_region_company)
 
-sns.heatmap(sales_by_region_company, annot=True, fmt='.2f')
+# 28 sns.heatmap(sales_by_region_company, annot=True, fmt='.2f')
+# plt.show()
+
+sales_by_date_df_five = df_five.groupby(['Date', 'Company'])['Sales'].sum().unstack()
+print(sales_by_date_df_five)
+print("-" * 20)
+sales_by_date_df_five = sales_by_date_df_five.fillna(0)
+print(sales_by_date_df_five)
+
+sales_by_date_df_five.plot(figsize=(12,6))
 plt.show()
-

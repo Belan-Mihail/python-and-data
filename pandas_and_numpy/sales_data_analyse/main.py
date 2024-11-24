134424269a334f8030bfbb13b2a2e139cb9375a5
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-11-24 20:31:27 +0100

    getting detailed information about specific companies

--- pandas_and_numpy/sales_data_analyse/main.py
+++ pandas_and_numpy/sales_data_analyse/main.py
@@ -163,10 +163,17 @@ df_five = df[df['Company'].isin(five_companies)]
 # группы_по_компаниям = df_фильтр.groupby('Company')['Sales'].agg(['sum', 'mean', 'max'])
 # print(группы_по_компаниям)
 
-sns.barplot(x='Company', y='Sales', data=df_five)
-plt.show()
-sns.boxplot(x='Company', y='Sales', data=df_five)
-plt.show()
+# sns.barplot(x='Company', y='Sales', data=df_five)
+# plt.show()
+# sns.boxplot(x='Company', y='Sales', data=df_five)
+# plt.show()
+
+for company in five_companies:
+    company_data = df[df['Company'] == company]
+    print(f'Company: {company}')
+    print(f'Region: {company_data['Region'].values[0]}')
+    print(f'Sales: {company_data['Sales'].sum()}')
+    print("-" * 20)
 
 # у меня есть сsv файл с такими колонками (Name,Region,Company,Date,Sales) со 100 строками данных. 
 # у меня есть сsv файл с такими колонками (Name,Region,Company,Date,Sales) со 100 строками данных. мне нужны задания средней сложности с данными которые содержаться в этом файле по пайтон пандас и матлиб для тренировки с решением, чтоб я мог свериться   

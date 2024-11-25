1d856daa30c778edeccc6d689600eacf853482f1
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-11-25 21:19:09 +0100

    Create a 3x4 array filled with 7

--- pandas_and_numpy/numpy/1/main.py
+++ pandas_and_numpy/numpy/1/main.py
@@ -12,6 +12,11 @@ zero_array = np.zeros((5,5))
 print(zero_array)
 print("-" * 20)
 
+# 2.1 Create a 3x4 array filled with 7
+sevens_array = np.full((3,4), 7)
+print(sevens_array)
+print("-" * 20)
+
 # 3 Create an array of 10 consecutive numbers from 5 to 15.
 array_from_5_to_15 = np.arange(5,16)
 print(array_from_5_to_15)

82d9a9a9d3662811b20a4b6c2703dbe276c3479c
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-11-25 21:12:54 +0100

    Create a 5x5 array filled with zeros

--- pandas_and_numpy/numpy/1/main.py
+++ pandas_and_numpy/numpy/1/main.py
@@ -4,3 +4,10 @@ import numpy as np
 # 1 Create an array of 10 random integers in the range 0 to 10
 random_array = np.random.randint(0, 11, size=10)
 print(random_array)
+print("-" * 20)
+
+
+# 2 Create a 5x5 array filled with zeros
+zero_array = np.zeros((5,5))
+print(zero_array)
+print("-" * 20)

f43038787057e88a85e03611d3c4743e30624226
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-11-29 14:02:19 +0100

    print df info and describe

--- pandas_and_numpy/cars_data_analyse/main.py
+++ pandas_and_numpy/cars_data_analyse/main.py
@@ -2,6 +2,14 @@ import pandas as pd
 
 # 1 Loading data from Cars.csv
 df = pd.read_csv('Cars.csv')
+print("-" *20)
 
-# Show first 5 Line of df
-print(df.head())
+# 2 Show first 5 Line of df
+print('df head \n', df.head())
+print("-" *20)
+
+# 3 print df info and description
+print('df info \n', df.info)
+print("-" *20)
+print('df describe \n', df.describe)
+print("-" *20)

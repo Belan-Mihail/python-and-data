10ba93cabd2ebadc2d48f92038356d7ad7f29a1c
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-02 22:06:56 +0100

    rename main.py into synt_sale_data and write dataframe to csv file

--- pandas_and_numpy/04 - demand_forecasting/synt_sales_data.py
+++ pandas_and_numpy/04 - demand_forecasting/synt_sales_data.py
@@ -16,7 +16,7 @@ for item_id in range (1, num_products + 1):
     for month in data_range:
         # Generating random data: sales, price, and holiday/promotion influence
         sales_volume = np.random.randint(50, 500) # random sales volume
-        price = round(np.random.randint(10, 10) 2) # random price
+        price = round(np.random.uniform(10, 10), 2) # random price
         holiday_influence = np.random.choice([0, 1], p=[0.7, 0.3])
         seasonality = np.sin(month.month * (np.pi / 6)) * 200
         sales_volume += seasonality + (holiday_influence * 100)
@@ -28,3 +28,9 @@ for item_id in range (1, num_products + 1):
         'price': price,
         'holiday_influence': holiday_influence
     })
+
+sales_data = pd.DataFrame(data)
+
+print(sales_data.head())
+
+sales_data.to_csv('sales_data.csv', index=False)

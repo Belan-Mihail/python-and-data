a9ec6abd22513608933c6f12878373d715fabb9a
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-02 23:08:08 +0100

    edit logic synt_sales_data.py

--- pandas_and_numpy/04 - demand_forecasting/synt_sales_data.py
+++ pandas_and_numpy/04 - demand_forecasting/synt_sales_data.py
@@ -3,9 +3,9 @@ import numpy as np
 
 # parameters for data generation
 num_products = 10 
-num_month = 6
+num_month = 12
 start_date = pd.to_datetime('2024-01-01') # starting data
-data_range = pd.date_range(start_date, periods=num_month, freq='ME')
+data_range = pd.date_range(start_date, periods=num_month, freq='MS')
 
 # generate data
 np.random.seed(42)
@@ -20,13 +20,15 @@ for item_id in range (1, num_products + 1):
         holiday_influence = np.random.choice([0, 1], p=[0.7, 0.3])
         seasonality = np.sin(month.month * (np.pi / 6)) * 200
         sales_volume += seasonality + (holiday_influence * 100)
+        marketing_spend = round(np.random.uniform(1000, 5000), 2)
     
     data.append({
         'item_id': item_id,
         'month': month.strftime("%Y-%m"),
         'sales_volume': max(0, round(sales_volume)),
         'price': price,
-        'holiday_influence': holiday_influence
+        'holiday_influence': holiday_influence,
+        'marketing_spend': marketing_spend
     })
 
 sales_data = pd.DataFrame(data)

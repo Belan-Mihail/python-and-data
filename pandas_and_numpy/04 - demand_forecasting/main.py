3ea6f107be9f51b7fad696cbe430bafc35d1611d
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-02 21:38:53 +0100

    add parameter for data generation

--- pandas_and_numpy/04 - demand_forecasting/main.py
+++ pandas_and_numpy/04 - demand_forecasting/main.py
@@ -1,1 +1,8 @@
-print('hello')
+import pandas as pd
+import numpy as np
+
+# parameters for data generation
+num_products = 10 
+num_month = 6
+start_date = pd.to_datetime('2024-01-01') # starting data
+data_range = pd.date_range(start_date, periods=num_month, freq='ME')

a5806daf6e6cf5892176ed192fa0bfb65c166988
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-03 08:53:34 +0100

    Mix up the months for each product

--- pandas_and_numpy/04 - demand_forecasting/add_random_to_data.py
+++ pandas_and_numpy/04 - demand_forecasting/add_random_to_data.py
@@ -9,3 +9,8 @@ df_sales = pd.read_csv('sales_data.csv')
 df_sales = df_sales.sample(frac=1, random_state=42)
 
 # print(df_sales.head())
+
+
+# Mix up the months for each product
+df_sales['month'] = df_sales.groupby('item_id')['month'].transform(np.random.permutation)
+# print(df_sales.head())

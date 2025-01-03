675a7d05929cdcdf708b6b615647d5beea4eaca1
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-03 20:39:00 +0100

    add sales_volume_noise column to df_sales

--- pandas_and_numpy/04 - demand_forecasting/add_random_to_data.py
+++ pandas_and_numpy/04 - demand_forecasting/add_random_to_data.py
@@ -56,3 +56,6 @@ def add_noise_holiday_influence(holiday_influence):
 df_sales['sales_volume'] = df_sales['sales_volume'].apply(add_noise_sales_volume)
 df_sales['price'] = df_sales['price'].apply(add_noise_price)
 df_sales['holoday_influence'] = df_sales['holoday_influence'].apply(add_noise_holiday_influence)
+
+# # To restore, we will add information about where and how the changes were made
+df_sales['sales_volume_noise'] = df_sales['sales_volume'] / df_sales['sales_volume'].apply(lambda x: add_noise_sales_volume(x))

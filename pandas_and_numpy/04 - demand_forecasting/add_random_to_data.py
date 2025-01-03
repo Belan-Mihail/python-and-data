adc6af60daf303b41d3fb56fb31571403c941e0c
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-03 09:14:45 +0100

    change date format in month column for 20% data

--- pandas_and_numpy/04 - demand_forecasting/add_random_to_data.py
+++ pandas_and_numpy/04 - demand_forecasting/add_random_to_data.py
@@ -13,9 +13,14 @@ df_sales = df_sales.sample(frac=1, random_state=42)
 
 # Mix up the months for each product
 df_sales['month'] = df_sales.groupby('item_id')['month'].transform(np.random.permutation)
-# print(df_sales.head())
+print(df_sales.head())
 
 # Entering erroneous data with a different month format
 # 20% of months will be converted to the format "%d-%m-%Y"
 num_rows = len(df_sales)
-incorecct_format_ind_1 = random.sample(range(num_rows), int(num_rows * 0,2))
+incorecct_format_ind_1 = random.sample(range(num_rows), int(num_rows * 0.2))
+
+for idx in incorecct_format_ind_1:
+    df_sales.loc[idx, 'month'] = pd.to_datetime(df_sales.loc[idx, 'month'], format='%Y-%m').strftime('%d-%m-%Y')
+
+print(df_sales.head())

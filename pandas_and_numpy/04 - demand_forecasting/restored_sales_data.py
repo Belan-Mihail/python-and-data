c8f158f05fc1674452d4807b6126c9b17707cf86
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-04 06:26:17 +0100

    Checking data type information

--- pandas_and_numpy/04 - demand_forecasting/restored_sales_data.py
+++ pandas_and_numpy/04 - demand_forecasting/restored_sales_data.py
@@ -7,3 +7,8 @@ df_sales = pd.read_csv('sales_data_with_noise.csv')
 print('First few rows')
 print(df_sales.head(10))
 print('-' * 20)
+
+# Checking data type information
+print('Data type information')
+print(df_sales.info())
+print('-' * 20)

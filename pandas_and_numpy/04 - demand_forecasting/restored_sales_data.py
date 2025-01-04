accecb8c8e8f345ff091c6222307a15dd636c11b
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-04 06:24:36 +0100

    view first few rows of data

--- pandas_and_numpy/04 - demand_forecasting/restored_sales_data.py
+++ pandas_and_numpy/04 - demand_forecasting/restored_sales_data.py
@@ -2,3 +2,8 @@ import pandas as pd
 
 # Read dataset
 df_sales = pd.read_csv('sales_data_with_noise.csv')
+
+# View the first few rows of data
+print('First few rows')
+print(df_sales.head(10))
+print('-' * 20)

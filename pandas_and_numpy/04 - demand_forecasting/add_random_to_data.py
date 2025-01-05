17fbebd0f63236a7a549f32585dda75559a0a5eb
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-05 20:02:50 +0100

    add noisea to sales_volume and add to df sales_volume_noise

--- pandas_and_numpy/04 - demand_forecasting/add_random_to_data.py
+++ pandas_and_numpy/04 - demand_forecasting/add_random_to_data.py
@@ -87,6 +87,7 @@ def add_noise_marketing_spend(spend):
 # Applying distortions to data
 # df_sales['sales_volume'] = df_sales['sales_volume'].apply(add_noise_sales_volume)
 # df_sales['price'] = df_sales['price'].apply(add_noise_price)
+df_sales['sales_volume'], df_sales['sales_volume_noise'] = zip(*df_sales['sales_volume'].apply(add_sales_volumes_noise_and_save_coefficient))
 df_sales['holiday_influence'] = df_sales['holiday_influence'].apply(add_noise_holiday_influence)
 df_sales['marketing_spend'] = df_sales['marketing_spend'].apply(add_noise_marketing_spend)
 

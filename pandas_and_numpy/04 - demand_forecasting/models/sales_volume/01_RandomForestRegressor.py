63593212d71ebf937e019f410cc6c1941678861d
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-06 09:48:19 +0100

    Define features and target variable

--- pandas_and_numpy/04 - demand_forecasting/models/sales_volume/01_RandomForestRegressor.py
+++ pandas_and_numpy/04 - demand_forecasting/models/sales_volume/01_RandomForestRegressor.py
@@ -6,4 +6,6 @@ from sklearn.preprocessing import StandardScaler
 
 df_sales = pd.read_csv('restored_sales_data.csv')
 
-print(df_sales.head())
+# Define features and target variable
+X = df_sales[['item_id', 'month', 'price', 'holiday_influence', 'marketing_spend']]
+y = df_sales['sales_volume']

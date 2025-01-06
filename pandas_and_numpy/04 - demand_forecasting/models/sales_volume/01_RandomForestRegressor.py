f43472d99c9f2dbff020ce0054e5a7d4d78a911b
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-06 09:41:34 +0100

    read csv restored_sales_data

--- pandas_and_numpy/04 - demand_forecasting/models/sales_volume/01_RandomForestRegressor.py
+++ pandas_and_numpy/04 - demand_forecasting/models/sales_volume/01_RandomForestRegressor.py
@@ -3,3 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
+
+df_sales = pd.read_csv('restored_sales_data.csv')
+
+print(df_sales.head())

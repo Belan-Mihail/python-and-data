3eebc4a0df4f6e229b3e86cbe2f9fe29cca5674d
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-06 09:50:06 +0100

    Divide the data into training and testing samples

--- pandas_and_numpy/04 - demand_forecasting/models/sales_volume/01_RandomForestRegressor.py
+++ pandas_and_numpy/04 - demand_forecasting/models/sales_volume/01_RandomForestRegressor.py
@@ -9,3 +9,6 @@ df_sales = pd.read_csv('restored_sales_data.csv')
 # Define features and target variable
 X = df_sales[['item_id', 'month', 'price', 'holiday_influence', 'marketing_spend']]
 y = df_sales['sales_volume']
+
+# Divide the data into training and testing samples
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)

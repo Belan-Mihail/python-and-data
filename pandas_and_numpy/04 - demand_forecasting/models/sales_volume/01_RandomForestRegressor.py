d60fc529a6dd25ee583cc0b16e3193f8ed49e32c
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-06 09:53:49 +0100

    Create and train a model

--- pandas_and_numpy/04 - demand_forecasting/models/sales_volume/01_RandomForestRegressor.py
+++ pandas_and_numpy/04 - demand_forecasting/models/sales_volume/01_RandomForestRegressor.py
@@ -12,3 +12,7 @@ y = df_sales['sales_volume']
 
 # Divide the data into training and testing samples
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create and train a model
+model = RandomForestRegressor(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)

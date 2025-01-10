a7794252eea78b11ee0bdc93560f1724b673225f
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-10 09:05:24 +0100

    synt data for each products and months in prognose year

--- pandas_and_numpy/04 - demand_forecasting/models/sales_volume/prognoses/prognose_2025.py
+++ pandas_and_numpy/04 - demand_forecasting/models/sales_volume/prognoses/prognose_2025.py
@@ -9,3 +9,15 @@ model = joblib('sales_model.pkl')
 num_product = 10
 future_year = 2025
 future_data = []
+
+# generate synt data for each products for each month in next year
+for item_id in range(1, num_product + 1):
+    for month in range(1, 13):
+        future_data.append({
+            'item_id': item_id,
+            'year': future_year,
+            'month_number': month,
+            'price': round(np.random.uniform(15, 20), 2),
+            'holiday_influence': np.random.choice([0, 1], p=[0.7, 0.3]),
+            'marketing_spend': round(np.random.uniform(1000, 5000), 2) 
+        })

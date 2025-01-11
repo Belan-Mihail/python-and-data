4568cc49acf8674114e0a879c8278847db99fe00
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-11 23:01:49 +0100

    loading data and transform categorical variables

--- pandas_and_numpy/04 - demand_forecasting/models/sales_volume/02_GradientBoostingRegressor.py
+++ pandas_and_numpy/04 - demand_forecasting/models/sales_volume/02_GradientBoostingRegressor.py
@@ -4,3 +4,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import LabelEncoder
+
+# Loading cleaned data
+df_sales = pd.read_csv('restored_sales_data.csv')
+
+# Transform categorical variables
+label_encoder = LabelEncoder()

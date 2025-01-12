25786561d32df15bdfc0438e643ab5e00c14b91c
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-12 20:45:34 +0100

    training model

--- pandas_and_numpy/04 - demand_forecasting/models/sales_volume/02_GradientBoostingRegressor.py
+++ pandas_and_numpy/04 - demand_forecasting/models/sales_volume/02_GradientBoostingRegressor.py
@@ -30,3 +30,5 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # model initialization
 model = GradientBoostingRegressor(n_estimators=100, random_state=42)
 
+# training model
+model.fit(X_train, y_train)

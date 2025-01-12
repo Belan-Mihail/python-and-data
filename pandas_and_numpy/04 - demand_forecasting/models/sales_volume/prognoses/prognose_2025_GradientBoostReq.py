7bbf7c97b09f62abf387b949ee1b41e8cc3a2925
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-12 21:29:59 +0100

    generate synth data for 2025 year

--- pandas_and_numpy/04 - demand_forecasting/models/sales_volume/prognoses/prognose_2025_GradientBoostReq.py
+++ pandas_and_numpy/04 - demand_forecasting/models/sales_volume/prognoses/prognose_2025_GradientBoostReq.py
@@ -7,3 +7,14 @@ model = joblib.load('sales_PM_GradientBoostingRegressor.pkl')
 # generation data for next 2025
 num_product = 10
 future_data = []
+
+# generate synt data for each products for each month in next year
+for item_id in range(1, num_product + 1):
+    for month in range(1, 13):
+        future_data.append({
+            'item_id': item_id,
+            'month_num': month,
+            'price': round(np.random.uniform(15, 20), 2),
+            'holiday_influence': np.random.choice([0, 1], p=[0.7, 0.3]),
+            'marketing_spend': round(np.random.uniform(1000, 5000), 2) 
+        }) 

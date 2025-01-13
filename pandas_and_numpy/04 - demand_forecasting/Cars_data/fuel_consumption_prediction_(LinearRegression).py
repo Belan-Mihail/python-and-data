cfad0a8341cd415694d97cb3dd2769ef35b86d5c
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-13 10:53:29 +0100

    read csv

--- pandas_and_numpy/04 - demand_forecasting/Cars_data/fuel_consumption_prediction_(LinearRegression).py
+++ pandas_and_numpy/04 - demand_forecasting/Cars_data/fuel_consumption_prediction_(LinearRegression).py
@@ -2,3 +2,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+# Read dataset
+df = pd.read_csv('./Cars_data/Cars.csv')
+

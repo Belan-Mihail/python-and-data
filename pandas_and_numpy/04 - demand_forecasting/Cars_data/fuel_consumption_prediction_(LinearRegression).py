0dc6951a70601dc2fa44700558e44e7deabd9946
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-13 10:55:51 +0100

    Features (X) and target variable (y)

--- pandas_and_numpy/04 - demand_forecasting/Cars_data/fuel_consumption_prediction_(LinearRegression).py
+++ pandas_and_numpy/04 - demand_forecasting/Cars_data/fuel_consumption_prediction_(LinearRegression).py
@@ -6,3 +6,7 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error
 # Read dataset
 df = pd.read_csv('./Cars_data/Cars.csv')
 
+# Features (X) and target variable (y)
+X = df.drop('mpg', axis=1)
+y = df['mpg']
+

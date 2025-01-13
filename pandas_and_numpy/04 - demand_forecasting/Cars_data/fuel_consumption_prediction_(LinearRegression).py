e0be65c6a9bd4cef757950e2d39b1beb0ecfa3d1
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-13 10:50:09 +0100

    create fuel_consumption_prediction_(LinearRegression) and make needly imports

---
+++ pandas_and_numpy/04 - demand_forecasting/Cars_data/fuel_consumption_prediction_(LinearRegression).py
@@ -0,0 +1,4 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error

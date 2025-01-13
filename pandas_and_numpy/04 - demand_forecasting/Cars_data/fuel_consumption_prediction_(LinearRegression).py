c26400fc8051183f1ec54d242ee0a43382488eec
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-13 11:00:14 +0100

    Model creation and training

--- pandas_and_numpy/04 - demand_forecasting/Cars_data/fuel_consumption_prediction_(LinearRegression).py
+++ pandas_and_numpy/04 - demand_forecasting/Cars_data/fuel_consumption_prediction_(LinearRegression).py
@@ -12,3 +12,7 @@ y = df['mpg']
 
 # Splitting data into training and testing samples
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Model creation and training
+model = LinearRegression()
+model.fit(X_train, y_train)

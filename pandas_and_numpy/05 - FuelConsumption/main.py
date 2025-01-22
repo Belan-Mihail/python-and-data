e9cce2685ef7cfaa18529678feeffe6ae18f6750
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-22 10:38:41 +0100

    replace missing values in Litters and Cost with 0

--- pandas_and_numpy/05 - FuelConsumption/main.py
+++ pandas_and_numpy/05 - FuelConsumption/main.py
@@ -7,4 +7,8 @@ print(os.getcwd())
 # load data from csv
 data = pd.read_csv('05 - FuelConsumption/FuelConsumption.csv', parse_dates=['Date'], dayfirst=True)
 
-print(data.head())
+# print(data.head())
+
+# Replace missing values ​​with 0
+data['Liters'] = data['Liters'].fillna(0)
+data['Cost'] = data['Cost'].fillna(0)

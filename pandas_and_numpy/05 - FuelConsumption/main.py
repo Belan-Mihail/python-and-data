630834e3f79bd1fa75c154f72e4e51be11ace288
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-22 10:42:12 +0100

    Replace negative values ​​in the Odo_Diff column with 0

--- pandas_and_numpy/05 - FuelConsumption/main.py
+++ pandas_and_numpy/05 - FuelConsumption/main.py
@@ -12,3 +12,8 @@ data = pd.read_csv('05 - FuelConsumption/FuelConsumption.csv', parse_dates=['Dat
 # Replace missing values ​​with 0
 data['Liters'] = data['Liters'].fillna(0)
 data['Cost'] = data['Cost'].fillna(0)
+
+# Replace negative values ​​in the "Odo_Diff" column with 0
+data['Odo_Diff'] = data['Odo_Diff'].apply(lambda x: max(0, x))
+
+print(data.head(20))

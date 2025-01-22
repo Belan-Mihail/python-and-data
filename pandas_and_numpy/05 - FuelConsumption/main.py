9a647d828b379bf8ea4327cf7789d30109ed01ae
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-22 10:45:08 +0100

    perform additional preprocessing of the Odo_Diff column

--- pandas_and_numpy/05 - FuelConsumption/main.py
+++ pandas_and_numpy/05 - FuelConsumption/main.py
@@ -13,6 +13,12 @@ data = pd.read_csv('05 - FuelConsumption/FuelConsumption.csv', parse_dates=['Dat
 data['Liters'] = data['Liters'].fillna(0)
 data['Cost'] = data['Cost'].fillna(0)
 
+# Convert the column to a numeric type and replace non-numeric values ​​with NaN
+data['Odo_Diff'] = pd.to_numeric(data['Odo_Diff'], errors='coerce')
+
+# Replace Nan with 0
+data['Odo_Diff'] = data['Odo_Diff'].fillna(0)
+
 # Replace negative values ​​in the "Odo_Diff" column with 0
 data['Odo_Diff'] = data['Odo_Diff'].apply(lambda x: max(0, x))
 

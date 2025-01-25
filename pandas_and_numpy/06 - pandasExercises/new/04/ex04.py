4d1294ea8297940ee63415b3f58458b112eacfbe
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-25 16:49:48 +0100

    complete steps 1-4

--- pandas_and_numpy/06 - pandasExercises/new/04/ex04.py
+++ pandas_and_numpy/06 - pandasExercises/new/04/ex04.py
@@ -2,8 +2,16 @@
 # Step 1. Go to https://www.kaggle.com/openfoodfacts/world-food-facts/data
 # Step 2. Download the dataset to your computer and unzip it.
 # Step 3. Use the tsv file and assign it to a dataframe called food
- 
+import pandas as pd
+import os
+
+
+current_directory = os.getcwd() 
+print(current_directory)
+food = pd.read_csv('06 - pandasExercises/new/04/en.openfoodfacts.org.products.tsv', sep='\t')
+
 # Step 4. See the first 5 entries
+print(food.head())
  
 # Step 5. What is the number of observations in the dataset?
  
